ec216103fa723c9af9bfa89c787814b0c471a820
dodididop <digdemcokyasar@gmail.com>
2021-11-29 15:18:02 +0300

    First commit | try-catch-finally

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                 Console.WriteLine("Enter number!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number you enter: "+num);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: "+ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("The operation is done.");
+            }
+            
+        }
+    }
+}
