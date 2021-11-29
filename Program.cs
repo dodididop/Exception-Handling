72f8ab60e55e1959fe2af4fb8b31878cac705672
dodididop <digdemcokyasar@gmail.com>
2021-11-29 15:52:37 +0300

    Added new exceptions.

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ namespace Exception_Handling
     {
         static void Main(string[] args)
         {
-            try
-            {
-                 Console.WriteLine("Enter number!");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The number you enter: "+num);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: "+ex.Message.ToString());
-            }
+            // try
+            // {
+            //     Console.WriteLine("Enter number!");
+            //     int num = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("The number you enter: "+num);
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine("Error: "+ex.Message.ToString());
+            // }
             // finally
             // {
             //     Console.WriteLine("The operation is done.");
@@ -23,14 +23,29 @@ namespace Exception_Handling
 
             try
             {
-                 int a = int.Parse(null);
+                 //int a = int.Parse(null);
+                 //int a = int.Parse("test");
+                 int a = int.Parse("-10000000000");
+
             }
             catch (ArgumentNullException ex)
             {
                 Console.WriteLine("Entered null value.");
                 Console.WriteLine(ex);
             }
-            
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Value is not appropriate.");
+                Console.WriteLine(ex);
+            }  
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Entered a value that is either too large or too small.");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("The program run succesfully.");
+            }          
         }
     }
 }
